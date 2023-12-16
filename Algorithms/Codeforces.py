f870751b0a352b0b836d6e14d1e2a9f828cf1ecf
Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>
2023-12-16 17:13:21 +0300

    Update Codeforces.py
    Signed-off-by: Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>

--- Algorithms/Codeforces.py
+++ Algorithms/Codeforces.py
@@ -1,2 +1,19 @@
 #https://codeforces.com/problemset/problem/1335/A
-помогите решить задачу
+# solve 1
+# for n in[*open(0)][1:]:print(int(n)-1>>1)
+# solve 2
+# import math
+# for _ in range(int(input())):
+#     n = int(input())
+#     if(n>2):
+#         print(math.ceil(n/2)-1)
+#     else:
+#         print(0)
+# solve 3
+# n = int(input())
+# for i in range(n):
+#     a = int(input())
+#     if a % 2 == 0:
+#         print(a // 2 - 1)
+#     else:
+#         print(a // 2)
